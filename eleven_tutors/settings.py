35ee037cf1784a83607524c2a10c63c28b7c4058
Otabek Narz <otabeknarz@gmail.com>
2025-09-13 12:31:32 +0500

    used dj_database_url

--- eleven_tutors/settings.py
+++ eleven_tutors/settings.py
@@ -11,6 +11,8 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 """
 import os
 from pathlib import Path
+
+import dj_database_url
 from dotenv import load_dotenv
 
 from datetime import timedelta
@@ -94,10 +96,7 @@ WSGI_APPLICATION = "eleven_tutors.wsgi.application"
 # https://docs.djangoproject.com/en/5.0/ref/settings/#databases
 
 DATABASES = {
-    "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
-    }
+    "default": dj_database_url.config(default=os.getenv("DATABASE_URL"))
 }
 
 
